f2a36c4c5fccc5547ac64b0d05b4eb2b62ce8b7b
Yusuf Jamal <yusufjamal1372@gmail.com>
2020-06-15 18:43:27 +0530

    Initialized the repo

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.view.dice_image
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener(){
+            rollDice()
+        }
+    }
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        when ((1..6).random()) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+    }
+}
+
