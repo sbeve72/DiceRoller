75a073f7299d431cf0c706217862fa83a1db3836
Yusuf Jamal <yusufjamal1372@gmail.com>
2020-09-29 02:19:26 +0530

    Now shows an on screen alert dialog when the change theme option is selected from the overflow menu.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.diceroller.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +19,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var diceImage: ImageView
     private lateinit var sharedPreferences: SharedPreferences
     private var diceFaceNumber: Int = 0
-    private var clickedItemId by Delegates.notNull<Int>()
+    private var selectedThemeOption by Delegates.notNull<Int>()
+    private lateinit var changeThemeDialog: MaterialAlertDialogBuilder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.activityToolbar)
+
         diceImage = binding.diceImage
         sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
-
         diceFaceNumber = savedInstanceState?.getInt("value") ?: 0
-        clickedItemId = sharedPreferences.getInt("clickedItemId", R.id.follow_system)
 
-        themeChanger(clickedItemId)
+        selectedThemeOption = sharedPreferences.getInt("selected_theme", 0)
+        themeChanger(selectedThemeOption)
+
+        changeThemeDialog = MaterialAlertDialogBuilder(this)
+        changeThemeDialog.setTitle(R.string.change_theme)
 
         if (diceFaceNumber != 0) {
             imageResourceSetter(diceFaceNumber)
@@ -49,23 +54,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreateOptionsMenu(menu)
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.action_bar, menu)
-        menu.findItem(clickedItemId).isChecked = true
         return true
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.follow_system || item.itemId == R.id.light_mode || item.itemId == R.id.dark_mode) {
-            sharedPreferencesSaver(item)
-            themeChanger(item.itemId)
-            item.isChecked = true
-            return true
+        when (item.itemId) {
+            R.id.change_theme -> {
+                changeThemeDialog.setSingleChoiceItems(
+                    arrayOf("Follow System", "Light Mode", "Dark Mode"),
+                    sharedPreferences.getInt("selected_theme", 0)
+                ) { dialogInterface, selectedOption ->
+                    selectedThemeInfoSaver(selectedOption)
+                    themeChanger(selectedOption)
+                    dialogInterface.dismiss()
+                }.show()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun sharedPreferencesSaver(item: MenuItem) {
+    private fun selectedThemeInfoSaver(selectedTheme: Int) {
         val editor = sharedPreferences.edit()
-        editor.putInt("clickedItemId", item.itemId)
+        editor.putInt("selected_theme", selectedTheme)
         editor.apply()
     }
 
@@ -74,11 +83,11 @@ class MainActivity : AppCompatActivity() {
         outState.putInt("value", diceFaceNumber)
     }
 
-    private fun themeChanger(menuItemId: Int) {
-        when (menuItemId) {
-            R.id.follow_system -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            R.id.light_mode -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            R.id.dark_mode -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    private fun themeChanger(selectedTheme: Int) {
+        when (selectedTheme) {
+            0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
     }
 
